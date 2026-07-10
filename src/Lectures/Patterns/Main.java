2459a7916e896a2e6d157be26e59f6ee2a167a5c
Akepati Sathish Reddy <sathishreddyakepati@gmail.com>
2026-07-10 22:04:02 +0530

    Added 4 more patterns

--- src/Lectures/Patterns/Main.java
+++ src/Lectures/Patterns/Main.java
@@ -2,9 +2,53 @@ package Lectures.Patterns;
 
 public class Main {
     public static void main(String[] args) {
-        pattern7(5);
+        pattern31(4);
 
     }
+    static void pattern31(int n){
+        int originalN = n;
+        n=n*2;
+        for(int row = 0;row<n;row++){
+            for(int col=0;col<n;col++){
+                int atEveryIndex =originalN- Math.min(Math.min(row,col),Math.min(n-row,n-col));
+                System.out.print(atEveryIndex+" ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern17(int n){
+        for(int row = 1;row<2*n-1;row++){
+            int c = row>n? 2*n-row:row;
+            for(int s = 1;s<=n-c;s++){
+                System.out.print(" ");
+            }
+
+            for(int col=c;col>=1;col--){
+                System.out.print(col);
+            }
+            for(int col= 2;col<=c;col++){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+    static void pattern30(int n){
+        for(int row = 1;row<=n;row++){
+            //spaces
+            for(int s = 1;s<=2*n-2*row;s++){
+                System.out.print(" ");
+            }
+
+            for(int col=row;col>=1;col--){
+                System.out.print(col+" ");
+            }
+            for(int col= 2;col<=row;col++){
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+    }
+
     static void pattern1(int n){
         for(int row = 1;row<=n;row++){
             for(int col = 1;col<=n;col++){
@@ -83,5 +127,21 @@ public class Main {
             System.out.println();
         }
 
+    }
+
+    static void pattern28(int n){
+
+        for(int row=0;row<2*n;row++){
+            int totalColsInRow = row > n ? 2 * n-row:row ;
+            int spaces = n - totalColsInRow;
+            for(int s = 1;s<=Math.abs(n-row);s++){
+                System.out.print(" ");
+            }
+            for(int col=0;col<totalColsInRow;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
     }
 }
